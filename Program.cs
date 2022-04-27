96853319aea210c4bc969205399e450ff189a7c5
PatrykStepien27 <patrykl655@wp.pl>
2022-04-27 22:20:25 +0200

    Mud blazor

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using EquationCalculator.Data;
+using MudBlazor.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMudServices();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
